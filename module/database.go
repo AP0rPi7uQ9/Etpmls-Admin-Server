ccc1edb3f49f1aa0874a073d3615af07a2760055
Samsung T5 - Program <Samsung T5 - Program>
2020-09-02 17:12:45 +0800

    note

--- module/database.go
+++ module/database.go
@@ -5,6 +5,9 @@ import (
 	"Etpmls-Admin-Server/database"
 )
 
+// Registration database
+// 注册数据库
+// var migrateList = []interface{}{&cms_database.Category{}, &cms_database.Post{}, &cms_database.Variable{}}
 var migrateList = []interface{}{}
 
 func initDatabase()  {
@@ -14,6 +17,8 @@ func initDatabase()  {
 	}
 }
 
+// Insert initial data
+// 插入初始数据
 func InsertModuleDataToDatabase()  {
-
+	// cms_initialization.InsertCmsDataToDatabase()
 }
--- module/route.go
+++ module/route.go
@@ -2,10 +2,14 @@ package module
 
 import "github.com/gin-gonic/gin"
 
+// API routing of the registered module
+// 注册模块的API路由
 func Module_RouteApi(r *gin.Engine)  {
-
+	// Example:
+	// cms_route.Cms_RouteApi(r)
 }
 
 func Module_RouteWeb(r *gin.Engine)  {
-
+	// Example:
+	// cms_route.Cms_RouteWeb(r)
 }
