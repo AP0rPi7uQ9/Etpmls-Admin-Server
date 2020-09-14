aba91d62ed28711bb960f3bd080b74891fff407b
Samsung T5 - Program <Samsung T5 - Program>
2020-09-14 21:42:14 +0800

    Prepare write cache interface
    准备写缓存接口

--- library/interface.go
+++ library/interface.go
@@ -2,6 +2,7 @@ package library
 
 var (
 	Jwt_Token = Interface_Jwt(&JwtGo{MySigningKey: []byte(Config.App.Key)})
+	// Cache = Interface_Cache(&Redis)
 )
 
 
@@ -12,5 +13,12 @@ type Interface_Jwt interface {
 	ParseToken(string) (interface{}, error)
 	GetIdByToken(string) (uint, error)						// Get user ID
 	GetIssuerByToken(string) (issuer string, err error)		// Get Username
+}
+
+
+// Cache interface
+// 缓存接口
+type Interface_Cache interface {
+
 }
 
